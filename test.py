9b35b2cbbf9490475dd52383e5cebfbc076ae52e
tanmaygaikwad <52346740+tanmaygaikwad@users.noreply.github.com>
2021-09-27 19:48:12 +0200

    files added

---
+++ test.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request
+import numpy as np
+import json
+import soundfile
+import numpy as np
+import librosa
+import glob
+import os
+
+import pickle
+# all emotions on TESS dataset
+int2emotion = {
+    "neutral": "neutral",
+    "happy": "happy",
+    "sad": "sad",
+    "angry": "angry",
+    "fear": "fear",
+    "disgust": "disgust"
+}
+
+# we allow only these emotions
+AVAILABLE_EMOTIONS = {
+    "angry",
+    "sad",
+    "neutral",
+    "happy"
+}
+app = Flask(__name__)
+
+# Load the Model back from file
+Pkl_Filename = "Pickle_RL_Model.pkl"
+with open(Pkl_Filename, 'rb') as file:  
+    Pickled_LR_Model = pickle.load(file)
+
+
+def predict_emotion(file):
+	features = extract_feature(file, mfcc=True, chroma=True, mel=True)
+	predictions =  Pickled_LR_Model.predict([features])
+	return predictions
+
+def extract_feature(file_name, **kwargs):
+    """
+    Extract feature from audio file `file_name`
+        Features supported:
+            - MFCC (mfcc)
+            - Chroma (chroma)
+            - MEL Spectrogram Frequency (mel)
+            - Contrast (contrast)
+            - Tonnetz (tonnetz)
+        e.g:
+        `features = extract_feature(path, mel=True, mfcc=True)`
+    """
+    mfcc = kwargs.get("mfcc")
+    chroma = kwargs.get("chroma")
+    mel = kwargs.get("mel")
+    contrast = kwargs.get("contrast")
+    tonnetz = kwargs.get("tonnetz")
+    with soundfile.SoundFile(file_name) as sound_file:
+        X = sound_file.read(dtype="float32")
+        sample_rate = sound_file.samplerate
+        if chroma or contrast:
+            stft = np.abs(librosa.stft(X))
+        result = np.array([])
+        if mfcc:
+            mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            result = np.hstack((result, mfccs))
+        if chroma:
+            chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+            result = np.hstack((result, chroma))
+        if mel:
+            mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+            result = np.hstack((result, mel))
+        if contrast:
+            contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+            result = np.hstack((result, contrast))
+        if tonnetz:
+            tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+            result = np.hstack((result, tonnetz))
+    return result
+
+
+
+print(predict_emotion("C:/Users/Ankit/Downloads/tan_001_angry.wav"))
